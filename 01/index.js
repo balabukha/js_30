de13d40d5425cae638fff5af61e211bc550a4a58
balabukha <3909591@gmail.com>
2017-10-27 23:17:28 +0300

    dev1

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,15 @@
+// let key = document.querySelectorAll(`.key[data-key="${e.keyCode}"`);
+// console.log(key);
 
+window.addEventListener('keydown', playing);
+
+
+function playing(e){
+    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    let sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    console.log(key);
+    console.log(sound);
+    if (!sound) return;
+    sound.currentTime=0;
+    sound.play();
+}
