b76c18f542545e15836848931a02c41600b798e3
balabukha <3909591@gmail.com>
2017-10-31 00:12:43 +0300

    dev7_02

--- 02/index.js
+++ 02/index.js
@@ -5,10 +5,9 @@ let seconds = document.querySelector(".seconds");
 
 function updating(){
     let now = new Date();
-    let secondsNew = now.getSeconds()/60 * 360+90;
-        seconds.style.transform = `rotate(${secondsNew}deg)`;
-        seconds.style.color = `red`;
-        console.log(secondsNew)
+        seconds.style.transform = `rotate(${now.getSeconds()/60 * 360+90}deg)`;
+        minutes.style.transform = `rotate(${now.getMinutes()/60 * 360+90}deg)`;
+        hours.style.transform = `rotate(${now.getHours()/12 * 360+90}deg)`;
 }
 
 setInterval(updating, 1000);
