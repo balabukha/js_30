b57d78b445b8a6cd01064e5b572bbf312ae02f2d
balabukha <3909591@gmail.com>
2017-11-06 23:34:30 +0300

    dev4_03

--- 04/index.js
+++ 04/index.js
@@ -1,21 +1,48 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+console.log(endpoint);
+
+const cities = [];
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => cities.push(...data));
+
+console.log( cities);
+
+function findMatches(wordToMatch, cities){
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch , 'gi')
+        return place.city.match(regex) || place.state.match(regex)
+    })
+}
+
 let input = document.querySelector('input');
+let ul_class = document.querySelector('.ul_class');
 
-let lis = [].slice.call(document.querySelectorAll("li"));
-console.log(lis);
-
-let n = lis.map((li) => li.innerText.split(" "));
-console.log(n);
+console.log(input);
 
 
-input.oninput = function (e){
-    console.log(input.value);
+input.addEventListener('change', displayMatches);
 
-};
+function displayMatches(){
+    this.value;
+}
+
+//
+// let lis = [].slice.call(document.querySelectorAll("li"));
+// console.log(lis);
+//
+// let n = lis.map((li) => li.innerText.split(" "));
+// console.log(n);
+//
+//
+// input.oninput = function (e){
+//     console.log(input.value);
+//
+// };
 
 
 // let A = 'Andrey';
 //
-// let ul_class = document.querySelector('.ul_class');
 // console.log(ul_class);
 // ul_class.innerHTML = `<li>${A}</li>`
 
